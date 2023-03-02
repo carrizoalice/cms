dd55effe91e8fae22f9dc5de263645d57f3b4d6b
Alice <carrizoalice@gmail.com>
2023-03-01 23:23:00 -0300

    creo componente cms card

---
+++ libs/ui/src/domains/components/cms-card/cms-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-card',
+  templateUrl: './cms-card.component.html',
+  styleUrls: ['./cms-card.component.scss']
+})
+export class CmsCardComponent {
+
+}
