49c1e3508df5d4a785698a739124b8b128d7c5b0
Alice <carrizoalice@gmail.com>
2023-03-02 23:36:24 -0300

    feat: agrego variables viewport

--- libs/ui/src/domains/components/cms-card/cms-card.component.ts
+++ libs/ui/src/domains/components/cms-card/cms-card.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cms-card',
-  templateUrl: './cms-card.component.html',
-  styleUrls: ['./cms-card.component.scss']
+  templateUrl: './cms-card.component.html'
 })
 export class CmsCardComponent {
 
