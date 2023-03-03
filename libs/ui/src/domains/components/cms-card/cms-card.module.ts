2b91646405e88df2e65966acdf2f38459957ac1f
Alice <carrizoalice@gmail.com>
2023-03-03 00:18:58 -0300

    feat: organizo estructura styles

--- libs/ui/src/domains/components/cms-card/cms-card.module.ts
+++ libs/ui/src/domains/components/cms-card/cms-card.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import { CmsCardComponent } from './cms-card.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [CmsCardComponent],
-  imports: [ CommonModule, MatCardModule],
+  imports: [ CommonModule, MatCardModule, MatButtonModule],
   exports: [CmsCardComponent]
 })
 export class CardModule {}
