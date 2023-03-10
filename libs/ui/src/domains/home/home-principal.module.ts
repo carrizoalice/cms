6a02535aceff916628d94c3de3388fe3c29d8da4
Alice <carrizoalice@gmail.com>
2023-03-09 21:22:46 -0300

    feat: cambio icono boton al colapsar

--- libs/ui/src/domains/home/home-principal.module.ts
+++ libs/ui/src/domains/home/home-principal.module.ts
@@ -4,9 +4,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { CardModule } from '../components/cms-card/cms-card.module';
 import { HomeComponent } from './home.component';
+import { MatChipsModule } from '@angular/material/chips';
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, CardModule, MatButtonModule, MatTableModule],
+  imports: [
+    CommonModule,
+    CardModule,
+    MatButtonModule,
+    MatTableModule,
+    MatChipsModule,
+  ],
   exports: [HomeComponent],
 })
 export class HomePrincipalModule {}
--- libs/ui/src/domains/home/home.component.ts
+++ libs/ui/src/domains/home/home.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export interface Vegetable {
+  name: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['home.component.scss'],
 })
 export class HomeComponent {
-  constructor() {}
+  vegetables: Vegetable[];
+
+  constructor() {
+    this.vegetables = [
+      { name: 'apple' },
+      { name: 'banana' },
+      { name: 'strawberry' },
+      { name: 'orange' },
+      { name: 'kiwi' },
+      { name: 'cherry' },
+    ];
+  }
 }
